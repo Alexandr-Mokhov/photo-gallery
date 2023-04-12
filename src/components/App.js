0ef8a43a5e1b87b8b57ec4f2df197350fb076122
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-04-13 00:00:54 +0500

    feat: сделать верстку компонента регистрации

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import EditProfilePopup from './EditProfilePopup';
 import EditAvatarPopup from './EditAvatarPopup';
 import AddPlacePopup from './AddPlacePopup';
 import DeleteCardPopup from './DeleteCardPopup';
-// import Register from './Register';
+import Register from './Register';
 
 export default function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = useState(false);
@@ -150,7 +150,7 @@ export default function App() {
     <div className="page">
       <CurrentUserContext.Provider value={currentUser}>
         <Header />
-        {/* <Register /> */}
+        <Register />
         <Main
           onEditProfile={handleEditProfileClick}
           onAddPlace={handleAddPlaceClick}
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,13 @@
 
 
-// export default function Register() {
-
-// }
+export default function Register() {
+	return (
+		<form className="form">
+			<h2 className="form__title">Регистрация</h2>
+			<input className="form__input form__input_type_login" placeholder="Email" type="email"></input>
+			<input className="form__input form__input_type_password" placeholder="Пароль" type="password"></input>
+			<button className="form__button-submit">Зарегистрироваться</button>
+			<h3 className="form__link-login">Уже зарегистрированы? Войти</h3>
+		</form>	
+	)
+}
