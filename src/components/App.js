ffdbbed7cd4d58c7be3875e85401eb08c784c1c1
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-04-20 20:55:20 +0500

    feat: очередной коммит

--- src/components/App.js
+++ src/components/App.js
@@ -246,7 +246,7 @@ export default function App() {
               setNotificationText={setNotificationText}
             />
           } />
-          <Route path="/" element={
+          <Route path="/react-mesto-auth" element={
             <ProtectedRouteElement element={Main}
               loggedIn={loggedIn}
               onEditProfile={handleEditProfileClick}
