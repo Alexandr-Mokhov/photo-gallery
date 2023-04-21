c3d43ea5a6045c8d1990ba4f1880c4262c538118
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-04-21 11:28:47 +0500

    repush

--- src/components/App.js
+++ src/components/App.js
@@ -246,7 +246,7 @@ export default function App() {
               setNotificationText={setNotificationText}
             />
           } />
-          <Route path="/react-mesto-auth" element={
+          <Route path="/react-mesto-auth" element={ //для коммита и пуша на гитхабе
             <ProtectedRouteElement element={Main}
               loggedIn={loggedIn}
               onEditProfile={handleEditProfileClick}
