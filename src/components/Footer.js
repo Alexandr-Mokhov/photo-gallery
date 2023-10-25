6fd66acb6ffbcfa1cc3861caa8b5e3eac1609606
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-10-25 14:33:57 +0500

    feat: изменить оформление и логотип

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copiring">
-        &copy; {new Date().getFullYear()} Mesto Russia Мохов&nbsp;Александр
+        &copy; {new Date().getFullYear()} Фотогалерея Александр&nbsp;Мохов
       </p>
     </footer>
   );
