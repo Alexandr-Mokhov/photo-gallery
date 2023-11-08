f73b8f6961dee8ef1f22ea83a16e067cf96d6378
Alexandr Mokhov <mokhov.alexandr.86@yandex.ru>
2023-11-08 18:48:41 +0500

    feat: поправить футер

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copiring">
-        &copy; {new Date().getFullYear()} Фотогалерея Александр&nbsp;Мохов
+        &copy; {new Date().getFullYear()} Фотогалерея. Александр&nbsp;Мохов
       </p>
     </footer>
   );
